2d155d3f91c6bc48afe3d8bde5c078948353505b
Floor Harmsen <floor.harmsen@gmail.com>
2018-03-06 14:37:35 +0100

    the showpage of the farm, with al the information from figma in it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Farm.destroy_all
+
+# floor = Farm.create!(farm_name: "Floor's Farm", farmers_name: "Floor")
+# carrot = Ingredient.create!(name: "carrot", farm_id: floor.id)
+# potato = Ingredient.create!(name: "potato", farm_id: floor.id)
+
+farm1= Farm.new(location: "test"  ,farm_name: "farm 1" ,farmers_name: "Piet de Boer", description: "Absolutely Super Nice Farm")
+farm1.save!
+
+ingr = Ingredient.new( name: "testcarrot"  ,description: "Mixed colors"   ,
+  main_category: "Fruits & Vegetables "  ,category: "Carrots"   ,price: 500,  rating: "1",
+  available_quantity: 500000, farm_id: farm1.id  ,price_type: "unit1"   )
+
+ingr.save!
+
+ingr1 = Ingredient.new( name: "testpotato"  ,description: "Mixed colors"   ,
+  main_category: "Fruits & Vegetables "   ,category: "Potatoes"   ,price: 500,  rating: "1",
+  available_quantity: 500000, farm_id: farm1.id  ,price_type: "unit1"   )
+
+ingr1.save!
+
+ingr2 = Ingredient.new( name: "test strawnberry"  ,description: "Mixed colors"   ,
+  main_category: "Fruits & Vegetables "   ,category: "Strawberry"   ,price: 500,  rating: "1",
+  available_quantity: 500000, farm_id: farm1.id  ,price_type: "unit1"   )
+
+ingr2.save!
+
