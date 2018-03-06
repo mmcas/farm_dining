4f851fdf429d17fd218a159269ce139e3043748e
Floor Harmsen <floor.harmsen@gmail.com>
2018-03-06 18:28:13 +0100

    added filter for price and rating

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,9 +1,21 @@
 class IngredientsController < ApplicationController
 
   def index
-    @ingredients = Ingredient.all
-    @ingredients = Ingredient.search_by_category_and_main_category(params[:query])
+    # Search
+    if params[:query].present?
+      @ingredients = Ingredient.search_by_category_and_main_category(params[:query])
+    else
+      @ingredients = Ingredient.all
+    end
 
+    # Sorting
+    if params[:filter].present?
+      if params[:filter] == "Rating"
+        @ingredients = @ingredients.sort_by { |ingredient| -ingredient.rating }
+      elsif params[:filter] == "Price"
+        @ingredients = @ingredients.sort_by { |ingredient| ingredient.price }
+      end
+    end
   end
 
   def show
