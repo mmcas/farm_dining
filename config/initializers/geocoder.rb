fffcaf4716a912a40c2fab75a4a86db86d540c3c
Mark van Riet <mvan.riet@hotmail.com>
2018-03-07 15:59:59 +0100

    config for autocomplete and google maps + address autocomplete

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -19,4 +19,8 @@ Geocoder.configure(
   # Calculation options
    units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
+  lookup:    :google,
+  api_key:   ENV['AIzaSyA7Iiw-TjxeeS_ZMXkjv7d81KfxgIHdF6A'],
+  use_https: true,
+
 )
