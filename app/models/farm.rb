646392c4506ec55f5f5034d658356ed0b478bcbf
Mark van Riet <mvan.riet@hotmail.com>
2018-03-07 16:25:01 +0100

    resolved photo upload seed errors

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,6 +1,8 @@
 class Farm < ApplicationRecord
   has_many :ingredients, dependent: :destroy
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :farm_photo, PhotoUploader
+  # mount_uploader :avatar_photo, PhotoUploader
+
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
