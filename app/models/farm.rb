43565cba4ab890985796809e527e0a5ef4d127d6
Mark van Riet <mvan.riet@hotmail.com>
2018-03-07 12:08:32 +0100

    long and lat in farms DB. updated farms model for geocoding

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,4 +1,6 @@
 class Farm < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   mount_uploader :photo, PhotoUploader
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
