27710036006f5c817dc63ad4c315406c9eb9808d
Floor Harmsen <floor.harmsen@gmail.com>
2018-03-08 10:24:40 +0100

    user signup creates restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_one :restaurant
   mount_uploader :photo, PhotoUploader
+
+  after_save :create_restaurant_and_give_it_user_id, on: :create
+
+  private
+
+  def create_restaurant_and_give_it_user_id
+    # Create a new restaurant and give it the id of the signed up user
+    restaurant = Restaurant.create!(user: self)
+  end
 end
+
