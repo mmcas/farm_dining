0c779fe09eaa0e0135022c7272c3f21a1e7f640f
Madelene Casselbrant <madelene.casselbrant@gmail.com>
2018-03-13 18:45:21 +0100

    Small change in js

--- app/assets/javascripts/shoppingbasket.js
+++ app/assets/javascripts/shoppingbasket.js
@@ -4,7 +4,7 @@ if (elements) {
   elements.forEach (function (element) {
     element.addEventListener("click", function( event ) {
       // display the current click count inside the clicked div
-      const id = this.dataset.order_id
+      var id = this.dataset.order_id;
       document.querySelector(`[data-edit_id="${id}"]`).style.display = 'block';
     }, false)
   });
