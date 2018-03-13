33cbf03d7227ca29662b1143a911815ff7cf2bad
Mark van Riet <mvan.riet@hotmail.com>
2018-03-13 12:14:26 +0100

    changed total order sum to correctly show total value after js update in quantities

--- app/models/shopping_basket.rb
+++ app/models/shopping_basket.rb
@@ -3,6 +3,8 @@ class ShoppingBasket < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   def total_price
-    orders.to_a.sum(&:amount_cents)
+    total = 0
+    orders.to_a.each{ |order| total += (order.amount_cents * order.order_quantity)}
+      return total
   end
 end
